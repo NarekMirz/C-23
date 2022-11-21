b7033a024ac951413983229c3806f184c45c3ee9
Narek <mirzakhanyan99@list.ru>
2022-11-21 12:50:56 +0300

    Неготовое

---
+++ dz13/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+System.Console.WriteLine("Введите число больше 99 ");
+
+int myNum = int.Parse(Console.ReadLine());
+
+if(myNum < 100)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    int thirdNum = myNum % 10;
+    System.Console.WriteLine(thirdNum);
+}
+if (myNum > 999)
+{
+    int thirdNum = (myNum / 100) % 10;
+    System.Console.WriteLine(thirdNum);
+}
